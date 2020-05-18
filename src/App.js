18da6eb3d75d69b55a0d582fc8e5e1851ca2db32
evey_alabi <evesho.alabi@tropixel.com.ng>
2020-05-18 12:57:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useTable } from "react-table";
 import './App.css';
 
 function App() {
+  
+  const data = [
+    { firstName: "jane", lastName: "doe", age: 20 },
+    { firstName: "john", lastName: "smith", age: 21 }
+  ];
+
+  const columns = [
+    {
+      Header: "Name",
+      columns: [
+        {
+          Header: "First Name",
+          accessor: "firstName"
+        },
+        {
+          Header: "Last Name",
+          accessor: "lastName"
+        }
+      ]
+    },
+    {
+      Header: "Other Info",
+      columns: [
+        {
+          Header: "Age",
+          accessor: "age"
+        }
+      ]
+    }
+  ];
+
+  
+  
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
